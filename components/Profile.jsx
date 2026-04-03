621dc9654932951f062d4857f48a8aac5a0f31d0
Qumber Zaidi <Qumber.Zaidi@cix.csi.cuny.edu>
2026-04-02 22:35:01 -0400

    Remove react-rounded-image and fix deployment issue

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,21 +1,38 @@
-import React, { Component } from "react"
-import ReactRoundedImage from "react-rounded-image"
+import React from "react";
+import { Box } from "@mui/material";
 import Pic from '../src/assets/profilepic.png'
-import WordFlicker from "./WordFlicker"
+import WordFlicker from "./WordFlicker";
 
-export default class Profile extends Component {
-  render() {
-    return (
-      <div style={{ display: "flex" }}>
-        <ReactRoundedImage image={Pic}  
-          imageWidth="170"
-          imageHeight="170"
-          roundedSize="10"
-          borderRadius="90"
-          padding="59"
-          />
-          <WordFlicker/>
-      </div>
-    )
-  }
+export default function Profile() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: "24px",
+        flexWrap: "wrap",
+      }}
+    >
+      <Box
+        component="img"
+        src={Pic}
+        alt="Qumber Zaidi"
+        sx={{
+          width: 170,
+          height: 170,
+          objectFit: "cover",
+          borderRadius: "50%",
+          border: "3px solid rgba(255,255,255,0.15)",
+          boxShadow: "0 12px 30px rgba(0,0,0,0.35)",
+          transition: "all 0.3s ease",
+          "&:hover": {
+            transform: "scale(1.05)",
+            boxShadow: "0 18px 40px rgba(228,90,132,0.35)",
+          },
+        }}
+      />
+
+      <WordFlicker />
+    </div>
+  );
 }
